b66779c5cc3c9cc84505f12928b15aa8d17e7a9a
danielscf <fdanielsantacruz@gmail.com>
2024-10-20 17:16:50 -0500

    Spring boot setup

---
+++ src/main/java/pe/utp/promocion_empresarial/PromocionEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package pe.utp.promocion_empresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocionEmpresarialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PromocionEmpresarialApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pe/utp/promocion_empresarial/PromocionEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.utp.promocion_empresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocionEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
